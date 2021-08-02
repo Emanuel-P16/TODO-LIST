9b59a07094f6147958f62b39f2f3e75af105935c
Emanuel-P16 <palazon.emanuel@gmail.com>
2021-08-02 14:50:58 -0300

    Se agregó el input para agregar tareas, se agrego el quitar tareas

--- main.js
+++ main.js
@@ -1,2 +1,29 @@
-console.log("holamundo")
-console.log("que se yo")
+
+//Html Objects
+
+const list = document.querySelector(".lista")
+const submit = document.querySelector(".submit")
+
+//EventsListeners
+
+list.addEventListener("click", deleteTask)
+submit.addEventListener("click",addTask)
+
+//Functions
+
+function addTask(e){
+   // e.preventDefault()
+    const input = document.querySelector(".input")
+    const inputValue = input.value
+    const newElement = document.createElement("li");
+    newElement.innerText = inputValue
+    newElement.className = "element"
+    list.appendChild(newElement)
+    input.value = ""
+    
+}
+ function deleteTask(e){
+    const elementRemoved = e.target
+     list.removeChild(e.target)
+    
+}
