ca3cd342723606c178e8b1ac0a60eeb2316180b9
Emanuel-P16 <palazon.emanuel@gmail.com>
2021-08-02 20:01:14 -0300

    cambios hechos en casa

--- main.js
+++ main.js
@@ -15,11 +15,16 @@ function addTask(e){
    // e.preventDefault()
     const input = document.querySelector(".input")
     const inputValue = input.value
-    const newElement = document.createElement("li");
-    newElement.innerText = inputValue
-    newElement.className = "element"
-    list.appendChild(newElement)
-    input.value = ""
+    if (inputValue === "") {
+      alert ("ingrese un valor")
+    } else {
+      const newElement = document.createElement("li");
+      newElement.innerText = inputValue
+      newElement.className = "element"
+      list.appendChild(newElement)
+      input.value = ""
+    }
+    
     
 }
  function deleteTask(e){
