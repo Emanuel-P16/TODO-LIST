79101ce060fc3af3bc0d99d0b724043cd653396f
Emanuel-P16 <palazon.emanuel@gmail.com>
2021-08-05 15:53:53 -0300

    se agrego el boton de borrar, de editar, se mejoro la estructura del html

--- main.js
+++ main.js
@@ -5,19 +5,31 @@ const list = document.querySelector(".lista")
 
 const listElement = document.querySelector(".elementContainer")
 
-const deleteTsk = document.querySelectorAll(".deleteTsk")
+//const deleteTaskButton = document.querySelectorAll(".deleteTaskButton")
 
 const submit = document.querySelector(".submit")
 
-//deleteTsk.addEventListener("click", deleteTask)
+document.body.addEventListener("click",function(event){
+  if (event.target.className == "deleteTaskButton"){
+    console.log("asd")
+    event.target.parentElement.remove()
+  }
+})
+document.body.addEventListener("click",function(event){
+  if (event.target.className == "editTsk"){
+    console.log("soy edit")
+    
+  }
+})
+//deleteTaskButton.addEventListener("click", deleteTask)
 submit.addEventListener("click",addTask)
-deleteTsk.forEach(function(task){
+/*deleteTaskButton.forEach(function(task){
 
     task.addEventListener("click",function(task){
-      console.log(task.target.parentElement)
+      
       task.target.parentElement.remove()
     })
-})
+})*/
 
 //Functions
 
@@ -28,11 +40,46 @@ function addTask(e){
     if (inputValue === "") {
       alert ("ingrese un valor")
     } else {
-      const newElement = document.createElement("li");
-      newElement.innerText = inputValue
-      newElement.className = "element"
-      list.appendChild(newElement)
+
+      const newLi = document.createElement("li");
+      //newLi.innerText = inputValue
+      newLi.className = "element"
+      
+      const newDiv = document.createElement("div");
+      
+      newDiv.className="elementContainer"
+      
+      const newDelBtn = document.createElement("button");
+      newDelBtn.className = "deleteTaskButton"
+      newDelBtn.innerText = "X"
+
+      const newEditBtn = document.createElement("button");
+      newEditBtn.className = "editTsk"
+      newEditBtn.innerText = "E"
+
+      const newInput = document.createElement("input");
+      newInput.disabled = true
+      newInput.value = inputValue
+      newInput.style = "color: black"
+      newInput.type = "text"
+      newLi.appendChild(newInput)
+      newDiv.appendChild(newLi)
+      
+      newDiv.appendChild(newEditBtn)
+      newDiv.appendChild(newDelBtn)
+      
+      list.appendChild(newDiv)
+      
+      
       input.value = ""
+      /*const deleteTaskButton = document.querySelectorAll(".deleteTaskButton")
+      deleteTaskButton.forEach(function(task){
+
+        task.addEventListener("click",function(task){
+          
+          task.target.parentElement.remove()
+        })
+    })*/
     }
     
     
